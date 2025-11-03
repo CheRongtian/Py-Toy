429da0ae28fe995837fa3fbbeceb6ad9c358caa1
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-11-02 23:24:07 -0600

    add comments

--- InternetPractice/UDPclient.py
+++ InternetPractice/UDPclient.py
@@ -1,10 +1,12 @@
-from socket import*
+from socket import * # include python socket lib
 
 serverName = 'localhost'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence: ')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+clientSocket = socket(AF_INET, SOCK_DGRAM) # create UDP socket
+message = input('Input lowercase sentence: ') # get user key board input
+clientSocket.sendto(message.encode(), (serverName, serverPort)) 
+# attach server name, port to message; send into socket
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+# read reply data (bytes) from socket
+print(modifiedMessage.decode()) # print out received string and close socket
 clientSocket.close()
