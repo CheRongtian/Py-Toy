6c4cf005dd5a996e2889f65befacef4786b5a325
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-11-03 14:15:55 -0600

    add some comment

--- InternetPractice/UDPserver.py
+++ InternetPractice/UDPserver.py
@@ -1,10 +1,12 @@
 from socket import *
 
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+serverSocket = socket(AF_INET, SOCK_DGRAM) # create UDP socket
+serverSocket.bind(('', serverPort)) # bind socket to local port number 12000
 print("The server is ready to receive")
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
+while True: # loop forever
+    message, clientAddress = serverSocket.recvfrom(2048) 
+    # Read from UDP socket into message, getting client's address (client IP and port)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # seng upper case string back to this client
